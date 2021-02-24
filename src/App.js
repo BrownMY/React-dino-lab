dd5428b76129ec7ba1a88e24991831d110a3bddf
mbrown <tugtuj@gmail.com>
2021-02-24 18:33:51 -0500

    App.js - Changed to class based

--- src/App.js
+++ src/App.js
@@ -1,28 +1,40 @@
+import React, { Component } from 'react';
 import './App.css';
-import './Comment'
+import Comment from './Comment';
 
-function App() {
-  //name and store variable before return statement
-  const post = {
+class App extends Component {
+  state = {
     title: "Dinosaurs are awesome",
     author: "Stealthy Stegosaurus",
     body: "Check out this body property",
     comments: ["First!", "Great post", "Hire this author now!"]
-  };
-  //map returns an array
-  const comments = post.comments.map(comment=> (<Comment text={comment}/>))
+  }
+  render() {
+    const post = {
+      title: "Dinosaurs are awesome",
+      author: "Stealthy Stegosaurus",
+      body: "Check out this body property",
+      comments: ["First!", "Great post", "Hire this author now!"]
+    };
+    //map returns an array
+    const comments = post.comments.map(comment => (<Comment text={comment} />))
 
-  return (
-    <div className="App">
-      {/* Use curly brackets to access variables */}
-     <p>{post.title}</p>
-     <p>{post.author}</p>
-     <p>{post.body}</p>
-     <p>{post.comments[0]}</p>
 
-     { comments }
-    </div>
-  );
+    return (
+      <div className="App">
+        {/* Use curly brackets to access variables */}
+        <h1>{this.state.title}</h1>
+        <p>{post.author}</p>
+        <p>{post.body}</p>
+        <h3>Comments:</h3>
+        <p>{post.comments[0]}</p>
+
+        { comments}
+      //name and store variable before return statement
+      </div>
+  
+    );
+  }
 }
 
 export default App;
